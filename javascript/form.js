da0654a83efdc2063659181d4c49376f12e4b237
sondoshassan <sondos199628@gmail.com>
2020-03-07 05:45:09 +0200

    make form and game

---
+++ javascript/form.js
@@ -0,0 +1,17 @@
+
+function alertMassege(){
+  modal.style.display = 'block';
+}
+
+
+
+var modal = document.getElementById('myModal');
+
+
+var buttonOk = document.getElementById('ok');
+
+
+
+buttonOk.onclick = function() {
+  modal.style.display = 'none';
+};
