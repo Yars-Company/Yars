d6f042125ef325d705bfa3b8991dc4fe117a2c59
Ammar <ammar.badwan95@gmail.com>
2020-03-09 00:08:38 +0200

    finished javascript for cart

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,8 @@
 'use strict';
 
+// Global variables
 var cartItem = []; //Array to save all the added items of the cart
+var cartID = document.getElementById('yarsCart');
 
 // Function to create the first local storage item if it doesnt exist
 if (localStorage.itemInCart === undefined ) {
@@ -15,6 +17,67 @@ function retrieveLocalStorage(){
 }
 retrieveLocalStorage();
 
+// For loop to create each item inside the HTML file
+
+for (var i = 0; i < cartItem.length; i++){
+  var divEl = document.createElement('div');
+  cartID.appendChild(divEl);
+  divEl.className = 'itemInformation';
+  divEl.id = i;
+
+  // Now creating the image element
+
+  var itemPicture = document.createElement('img');
+  divEl.appendChild(itemPicture);
+  itemPicture.src = cartItem[i].url;
+  itemPicture.width = 150;
+  itemPicture.height = 150;
 
 
-console.log('hello');
+  // Add item info
+
+  var soapNameEl = document.createElement('span');
+  divEl.appendChild(soapNameEl);
+  soapNameEl.textContent = cartItem[i].name;
+  soapNameEl.className = 'nameOfSoap';
+
+  // Add item price
+
+  var priceEl = document.createElement('span');
+  divEl.appendChild(priceEl);
+  priceEl.textContent = cartItem[i].price + 'JOD';
+  priceEl.className = 'priceOfSoap';
+
+  // Add remove button
+
+  var buttonEl = document.createElement('button');
+  divEl.appendChild(buttonEl);
+  buttonEl.textContent = 'Remove';
+  buttonEl.className = 'removeButton';
+}
+
+// Make an array to with all buttons to choose from them
+var buttonElClass = document.getElementsByClassName('removeButton');
+
+// Adding event listeners to the buttons
+for (i = 0; i < cartItem.length ; i++){
+  var removeButton = buttonElClass[i];
+  removeButton.addEventListener('click', removingButton);
+}
+
+function removingButton(event){
+  var removeItem = event.target;
+  var itemID = removeItem.parentElement.id;
+  document.getElementById(itemID).remove();
+  cartItem.splice(itemID , 1);
+  updateCart();
+  console.log(itemID);
+}
+
+// Function to update local storage after removing an item
+
+function updateCart(){
+  localStorage.clear();
+  var jsonCart = JSON.stringify(cartItem);
+  localStorage.itemInCart = jsonCart;
+}
--- js/store.js
+++ js/store.js
@@ -7,6 +7,7 @@ var soapsDiv = document.getElementById('soapSales');
 var soapsNames = ['Soapy' , 'Saab', 'Baladi', 'Nabulsi', 'M3a6arah'];
 var soapsDescription = ['Very Soapy Soap', 'A Saab Soap' , 'The old traditional soap', 'Nabulsi soap 3\'anye 3an el ta3reef ','soap with nice smell'];
 var soapPicture = ['s1.jpg','s2.jpg','s3.jpg','soap4.jpg','soap5.jpg'];
+var soapPrice = [7,8,4,6,10];
 
 
 // Function to create the first local storage item if it doesnt exist
@@ -24,17 +25,18 @@ retrieveLocalStorage();
 
 
 // Constructor function to create soap item list
-function ListItems (name, description, url){
+function ListItems (name, description, url,price){
   this.name = name;
   this.description = description;
   this.url = `./img/${url}`;
   this.itemNumber = i;
+  this.price = price;
   availableItems.push(this);
 }
 
 // For loop to create all the soaps that are inside soapNames and their descriptions and to create the listing in HTML store page using DOM manipulation
 for (var i = 0; i<soapsNames.length;i++){
-  new ListItems (soapsNames[i],soapsDescription[i], soapPicture[i]);
+  new ListItems (soapsNames[i],soapsDescription[i], soapPicture[i],soapPrice[i]);
 
   // Now to create the item div itself and give it a unique ID
 
@@ -68,6 +70,12 @@ for (var i = 0; i<soapsNames.length;i++){
   itemDiv.appendChild(buttonEl);
   buttonEl.className = 'addToCartButton';
   buttonEl.textContent = 'Add to cart';
+
+  // Now creating the price tab
+
+  var priceEl = document.createElement('span');
+  itemDiv.appendChild(priceEl);
+  priceEl.textContent = availableItems[i].price + 'JOD';
 }
 // Make an array to with all buttons to choose from them
 var buttonElClass = document.getElementsByClassName('addToCartButton');
