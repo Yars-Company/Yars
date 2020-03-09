96e26b3cecee510bdedf84b0dd1c00fbb3ee1cad
Ammar <ammar.badwan95@gmail.com>
2020-03-09 14:13:03 +0200

    added functionality to cart page

--- js/cart.js
+++ js/cart.js
@@ -3,6 +3,8 @@
 // Global variables
 var cartItem = []; //Array to save all the added items of the cart
 var cartID = document.getElementById('yarsCart');
+var bottomHeader = document.getElementById('bottomHeader');
+var totalPrice = 0;
 
 // Function to create the first local storage item if it doesnt exist
 if (localStorage.itemInCart === undefined ) {
@@ -68,10 +70,39 @@ for (i = 0; i < cartItem.length ; i++){
 function removingButton(event){
   var removeItem = event.target;
   var itemID = removeItem.parentElement.id;
+  updateTotal(itemID);
   document.getElementById(itemID).remove();
   cartItem.splice(itemID , 1);
   updateCart();
   console.log(itemID);
+}
+
+// Total price
+
+function totalPriceFunction(){
+  for (i = 0; i < cartItem.length; i++) {
+    totalPrice = totalPrice + cartItem[i].price;
+  }
+  if (cartItem.length === 0) {
+    console.log('empty cart');
+  } else {
+    document.getElementById('emptyCartWarning').remove();
+    var botHeaderEl = document.createElement('p');
+    bottomHeader.appendChild(botHeaderEl);
+    botHeaderEl.id = 'totalTag';
+    bottomHeader.appendChild = totalPrice;
+    botHeaderEl.textContent = 'Your current price is: ' + totalPrice + ' JOD';
+    console.log('cart got items');
+  }
+}
+totalPriceFunction();
+
+// Updating total price after remove
+
+function updateTotal(itemID){
+  totalPrice = totalPrice - cartItem[itemID].price;
+  var updatedPrice = document.getElementById('totalTag');
+  updatedPrice.textContent = 'Your current price is: ' + totalPrice + ' JOD';
 }
 
 // Function to update local storage after removing an item
@@ -81,3 +112,18 @@ function updateCart(){
   var jsonCart = JSON.stringify(cartItem);
   localStorage.itemInCart = jsonCart;
 }
+
+// Add event listener to Clear all button
+
+var clearAll = document.getElementById('clearAll');
+clearAll.addEventListener('click', clearAllFunction);
+
+function clearAllFunction(){
+  var elementsToClear = document.getElementsByClassName('itemInformation');
+  while(elementsToClear[0]) {
+    elementsToClear[0].parentNode.removeChild(elementsToClear[0]);
+  }
+  localStorage.clear();
+  cartItem = [];
+}
+
